46a3421349fc3c4e3e87e404fee9d4be088b06bb
anupkrbid <anupbid94@gmail.com>
2017-10-20 23:57:12 +0530

    Implemented vuex in stocks

--- src/components/stocks/stocks.vue
+++ src/components/stocks/stocks.vue
@@ -7,18 +7,13 @@
 <script>
     import Stock from './stock.vue'
     export default {
-      data () {
-        return {
-          stocks: [
-            { id: 1, name: 'BMW', price: 150 },
-            { id: 1, name: 'Google', price: 250 },
-            { id: 1, name: 'Apple', price: 230 },
-            { id: 1, name: 'Twitter', price: 90 }
-          ]
-        }
-      },
       components: {
         appStock: Stock
+      },
+      computed: {
+        stocks () {
+          return this.$store.getters.stocks
+        }
       }
     }
 </script>
---
+++ src/data/stocks.js
@@ -0,0 +1,6 @@
+export default [
+  { id: 1, name: 'BMW', price: 150 },
+  { id: 1, name: 'Google', price: 250 },
+  { id: 1, name: 'Apple', price: 230 },
+  { id: 1, name: 'Twitter', price: 90 }
+]
